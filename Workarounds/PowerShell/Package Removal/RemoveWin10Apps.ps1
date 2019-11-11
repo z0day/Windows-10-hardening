4badd918abd2a7f6ac024da77763a992a77eea3e
CHEF-KOCH <CHEF-KOCH@protonmail.com>
2019-11-11 18:34:25 +0000

    Workaround folder added
    ALPHA 0.4

---
+++ Workarounds/PowerShell/Package Removal/RemoveWin10Apps.ps1
@@ -0,0 +1,49 @@
+﻿# FIXME
+
+$appxpackages = (
+	'3DBuilder',
+	'BingFinance',
+	'BingSports',
+	'CommsPhone',
+	'ConnectivityStore',
+	'GetHelp',
+	'Getstarted',
+	'HaloCamera',
+	'HaloItemPlayerApp',
+	'HaloShell',
+	'Messaging',
+	'Microsoft3DViewer',
+	'MicrosoftOfficeHub',
+	'MicrosoftSolitaireCollection',
+	'Office.Sway',
+	'OneConnect',
+	'People',
+	'Print3D',
+	'SkypeApp',
+	'WindowsFeedbackHub',
+	'WindowsPhone',
+	'Xbox.TCUI',
+	'XboxApp',
+	'ZuneMusic',
+	'ZuneVideo',
+	'windowscommunicationsapps'
+)
+
+ForEach ($package in $appxpackages)
+{
+	try
+ {
+		$packagenames = (Get-AppxProvisionedPackage -online | Where-Object { $_.DisplayName -like '*' + $package + '*' }).PackageName
+
+		ForEach ($packagename in $packagenames)
+		{
+			DISM /online /remove-provisionedappxpackage /packagename:$packagename
+		}
+	}
+	catch
+ {
+		Write-Host "Error: "
+		Write-Host $package
+	}
+}
+
